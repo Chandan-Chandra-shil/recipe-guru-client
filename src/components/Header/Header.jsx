c7791f74d1563db938baa6f073e5f9cf4f9c4fa8
Chandan-Chandra-shil <chandanchandrashil22@gmail.com>
2023-05-02 12:10:14 +0600

    add navnar and login form

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Button, Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div>
-      <Link to="/">Home</Link>
+      {/*  <Link to="/">Home</Link>
       <Link to="/blog">Blog</Link>
       <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
-
-      
+      <Link to="/register">Register</Link> */}
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Container>
+          <h1 className="text-warning fs-2">Recipe Guru</h1>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto gap-4 ">
+              <NavLink className="text-decoration-none fs-5" to="/">
+                Home
+              </NavLink>
+              <NavLink className="text-decoration-none fs-5" to="/blog">
+                Blog
+              </NavLink>
+            </Nav>
+            <Nav>
+              <Link to="/login">
+                <Button className="btn btn-warning ms-4 fw-semibold fs-5">
+                  Login
+                </Button>
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 };
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,50 @@
 import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div>
-      <h2>login page</h2>
-      
-    </div>
+    <Container className="mt-5 container mx-auto w-25 border rounded shadow  p-5">
+      <h2 className="fw-semibold">Please Login </h2>
+      <Form className=" ps-2">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button className="ms-3 px-4 mb-4" variant="primary" type="submit">
+          Login
+        </Button>
+        <div className="d-grid gap-2 mb-4">
+          <Form.Text>
+            <h2 className="text-center  text-secondary">
+              <small>Login with</small>
+            </h2>
+          </Form.Text>
+          <Button variant="outline-primary" size="lg">
+            Google
+          </Button>
+          <Button variant="outline-secondary" size="lg">
+            GitHub
+          </Button>
+        </div>
+        <Form.Text>
+          <p>
+            Don't have an Account? <Link to="/register">Register</Link>
+          </p>
+        </Form.Text>
+      </Form>
+    </Container>
   );
 };
 
